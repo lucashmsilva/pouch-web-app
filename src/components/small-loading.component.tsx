190f8913b811ee52181650fb546056d20abf4340
Lucas Silva <lucashenrique580@gmail.com>
2021-12-02 11:00:00 -0300

    creates new small loading spinner

---
+++ src/components/small-loading.component.tsx
@@ -0,0 +1,9 @@
+const SmallLoadingSpinner = () => (
+  <>
+  <div className="loadingio-spinner-spin-4ffxh03ubks"><div className="ldio-pazicmv58mg">
+  <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+  </div></div>
+  </>
+);
+
+export default SmallLoadingSpinner;
